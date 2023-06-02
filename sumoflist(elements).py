08996697146fb8fa3c5a23206b363b9df66ebca3
Shiddhant77 <103237037+Shiddhant77@users.noreply.github.com>
2023-06-02 16:38:57 -0400

    Add files via upload

---
+++ sumoflist(elements).py
@@ -0,0 +1,11 @@
+'''
+Write a Python program to find the sum of all the elements in a given list.
+
+'''
+
+list1= [1,2,4,5,6,7,8,3,4,5,6,4,3]
+sum = 0
+for num in (list1):
+    sum = sum + num
+
+print(sum)
